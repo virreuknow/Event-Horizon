5bc493435c087de972cca49f354e57196292f7fa
virreuknow <viktor.wahrb@gmail.com>
2025-05-05 09:33:51 +0200

    Gjorde fetchen tydligare

--- Hem/script.js
+++ Hem/script.js
@@ -1,14 +1,16 @@
 async function fetchLatestNews() {
     const [articlesRes, blogsRes] = await Promise.all([ //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise
-      fetch('https://api.spaceflightnewsapi.net/v4/articles/?limit=1&ordering=-published_at'),
-      fetch('https://api.spaceflightnewsapi.net/v4/blogs/?limit=1&ordering=-published_at') //https://api.spaceflightnewsapi.net/v4/docs/
+      fetch('https://api.spaceflightnewsapi.net/v4/articles/?limit=1&offset=0'),
+      fetch('https://api.spaceflightnewsapi.net/v4/blogs/?limit=1&offset=0') //https://api.spaceflightnewsapi.net/v4/docs/
     ]);
 
     const articleData = await articlesRes.json();
     const blogData = await blogsRes.json();
+    console.log(articleData.results[0].published_at)
 
-    let latest = articleData.results.concat(blogData.results)
-      .sort((a, b) => new Date(b.published_at) - new Date(a.published_at))[0];
+    const latest = ((articleData.results[0].published_at) > (blogData.results[0].published_at)) ? (articleData.results[0]) : (blogData.results[0])
+
+
 
     document.getElementById('breaking-news').innerHTML = `
       <a href="${latest.url}" target="_blank" style="color:white;text-decoration:none;display: block;">🚨 ${latest.title}</a>
